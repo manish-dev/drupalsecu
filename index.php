9e3aeb42ac01c22165582fa0a39f340ad2c6824f
manish-dev <manish.drupal@gmail.com>
2015-12-17 11:37:00 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <h1>Security Check</h1>
+        <form action="security.php" method="post">
+            <table width="500" cellpadding="10">
+                <tr>
+                    <td width="200"><label>Site name</label></td>
+                    <td width="300"><input name="site_name" type="text" placeholder="xyz.com" size="80" /></td>
+                </tr>
+                <tr>
+                    <td width="200"><label>Site folder name</label></td>
+                    <td width="300"><input name="site_folder_name" type="text" placeholder="xyz" size="80" /></td>
+                </tr>
+                <tr><td></td><td><input type="submit"></td></tr>
+            </table>
+        </form>
+    </body>
+</html>
